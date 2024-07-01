683ab9a46f1d1200af40c0cb2655900b8db889cb
abhaysingh73 <abhay73986@gmail.com>
2024-07-01 12:05:46 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const taskRoutes = require('./routes/tasks');
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://0.0.0.0/tasksdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+app.use('/api/tasks', taskRoutes);
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
